834775cf79b9eb2eae0312d8bb0d76469f7178ca
Nazakin <134816178+Nazakin@users.noreply.github.com>
2024-02-05 23:36:22 +0100

    .

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -6,7 +6,18 @@ function App() {
 
   return (
     <div className="App">
-     <ClassCounter/>
+     <div className="post">
+      <div className="post__content">
+        <strong>1. Javascript</strong>
+        <div>
+          JS - programming language
+        </div>
+      </div>
+      <div className="post__btns">
+        <button>delete</button>
+      </div>
+
+     </div>
     </div>
   );
 }
